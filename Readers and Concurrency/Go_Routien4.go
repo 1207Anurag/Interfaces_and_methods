8bd2cdc17f4b1d5b186f74bd9fedf61fc83a5104
Anurag Chaubey <anurag.chaubey@zopsmart.com>
2022-02-22 00:27:18 +0530

    Moved to the new folder

--- Readers and Concurrency/Go_Routien4.go
+++ Readers and Concurrency/Go_Routien4.go
@@ -23,7 +23,64 @@ func CallUrl(id string, wg *sync.WaitGroup) {
 		}
 	}
 }
-
+func CallUrl1(id string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	res, err := http.Get("https://jsonplaceholder.typicode.com/posts" + id)
+	if err != nil {
+		fmt.Println("error accessing URL")
+	} else {
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println("cannot read body of Response")
+		} else {
+			fmt.Println(string(body))
+		}
+	}
+}
+func CallUrl2(id string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	res, err := http.Get("https://jsonplaceholder.typicode.com/posts" + id)
+	if err != nil {
+		fmt.Println("error accessing URL")
+	} else {
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println("cannot read body of Response")
+		} else {
+			fmt.Println(string(body))
+		}
+	}
+}
+func CallUrl3(id string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	res, err := http.Get("https://jsonplaceholder.typicode.com/posts" + id)
+	if err != nil {
+		fmt.Println("error accessing URL")
+	} else {
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println("cannot read body of Response")
+		} else {
+			fmt.Println(string(body))
+		}
+	}
+}
+func CallUrl4(id string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	res, err := http.Get("https://jsonplaceholder.typicode.com/posts" + id)
+	if err != nil {
+		fmt.Println("error accessing URL")
+	} else {
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println("cannot read body of Response")
+		} else {
+			fmt.Println(string(body))
+		}
+	}
+}
+848170675
+1153234471
 func main() {
 	st := time.Now()
 	wg := new(sync.WaitGroup)
@@ -31,6 +88,10 @@ func main() {
 	for i := 1; i <= 200; i++ {
 		id := strconv.Itoa(i)
 		go CallUrl(id, wg)
+		go CallUrl1(id,wg)
+		go CallUrl2(id,wg)
+		go CallUrl3(id, wg)
+		go CallUrl4(id,wg)
 	}
 	wg.Wait()
 	diff := time.Since(st)
